bc2118e4595d0c9e2a88d3e6a174b17b6fe328ae
Rabib01 <muhtasim.rabib@northsouth.edu>
2026-03-13 19:28:05 +0600

    fixed a smallbug in else statement of controlAddBookmark function of controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -83,8 +83,7 @@ const controlServings = function (newServings) {
 
 const controlAddBookmark = function () {
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
-  else model.state.recipe.bookmarked;
-  model.deleteBookmark(model.state.recipe.id);
+  else model.deleteBookmark(model.state.recipe.id);
   console.log(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
